3e00f44ee125c4c95f9d3126c3a8501afbfcad1e
Marcos Rivas <marcosrivasr@gmail.com>
2022-02-23 22:21:08 -0600

    vite files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import "../css/main.css";
+import "../css/darklightmode.css";
+
 const moreOptions = document.querySelector("#bmore");
 const bShowMobileLinks = document.querySelector("#bmenu");
 const mobileMenu = document.querySelector(".links");
